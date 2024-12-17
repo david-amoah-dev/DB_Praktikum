a76db9a5f49c5aa507a3601fab0e9b0b42b6ebac
smoems-smoems <simon.oenings@gmail.com>
2024-12-17 15:03:30 +0100

    update

--- __init__.py
+++ __init__.py
@@ -73,3 +73,11 @@ def logout():
 
 
 
+if __name__ == "__main__":
+    # Create all tables within the application context
+    with app.app_context():
+        db.create_all()
+        
+
+    print("Database tables created!")
+    app.run(debug=True)
