b4844bcb70f8ae95c1a1532da89b13b670cb7347
saga <saghar.roshanavandi@stud.uni-due.de>
2024-12-20 13:04:50 +0100

    udgfulsf

--- app.py
+++ app.py
@@ -10,6 +10,49 @@ db = SQLAlchemy(model_class=Base)
 # create the app
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project1.db"
 # initialize the app with the extension
 db.init_app(app)
+
+
+class Resto(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), unique = False, nullable = False)
+    strasse = db.Column(db.String(20), unique = False, nullable = False)
+    plz = db.Column(db.Integer(), unique = False, nullable = False)
+    beschreibung = db.Column(db.String(555), nullable = False)
+    password = db.Column(db.String(20), nullable = False)
+    openTime = db.Column(db.String(), nullable = False)
+    wallet = db.Column(db.Integer, nullable=False, default=200)
+
+    def __repr__(self):
+       return f"Resto('{self.name}')"
+
+class Kunde(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    vorname = db.Column(db.String(20), unique = False, nullable = False)
+    nachname = db.Column(db.String(20), unique = False, nullable = False)
+    adresse = db.Column(db.String(20), unique = False, nullable = False)
+    postleitzahl = db.Column(db.Integer(), nullable = False)
+    password = db.Column(db.String(20), nullable = False)
+    wallet = db.Column(db.Integer, nullable=False, default=200)
+
+    def __repr__(self):
+       return f"Kunde('{self.nachname}')"
+with app.app_context():
+  db.create_all()
+
+    
+    
+@app.route('/', methods=['GET'])
+def CatchResto():
+    #catch the PLZ from session(for now the static version)
+    kunde_postleitzahl= 12345
+   
+
+    #catch the restaurant with the same PLZ
+    restaurants= Resto.query.filter_by(plz=kunde_postleitzahl).all()
+    return render_template('restaurants.html', restaurants = restaurants)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
