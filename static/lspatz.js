6104052f28d629843ce4e484359481a63b073c5b
david-amoah-dev <123707317+david-amoah-dev@users.noreply.github.com>
2025-01-05 00:00:31 +0100

    Add files via upload

---
+++ static/lspatz.js
@@ -0,0 +1,60 @@
+//This is going to be the main JavaScript files
+
+/* JavaScript Objects
+
+    Customer
+    function cstmobj(first, last, plz, adress, wallet) {
+    this.firstName = first;
+    this.lastName = last;
+    this.plz = plz;
+    this.adress = adress;
+    this.wallet = wallet;
+    update-function =
+    }
+
+    Restaurant
+    function rstrobj(restname, plz, adress, wallet) {
+    this.restname = restname;
+    this.plz = plz;
+    this.adress = adress;
+    this.wallet = wallet;
+    update-function =
+    }
+
+    Item
+    function itmobj(name, price, img, desc) {
+    this.name = name;
+    this.price = price;
+    this.img = img;
+    this.desc = desc;
+    update-function =
+    }
+
+    Order
+    function ordrobj(orderid, total, itemdet) {
+    this.orderid = orderid;
+    this.total = total;
+    this.itemid = img;
+    }
+
+*/
+
+/* JavaScript Classes
+
+    addToCart
+
+    purchase
+
+    confirmPurchase
+
+    setState
+
+    createNewItem
+
+    createNewOrder
+
+    editProfile -customer & restaurant #update
+
+
+
+*/
