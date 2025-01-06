458b2ac39da70bf8c8c4176bf10e70d8a72723d6
smoems-smoems <simon.oenings@gmail.com>
2025-01-06 16:35:37 +0100

    paar bugs hier und da

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import select
-from flask_session import Session
 
 class Base(DeclarativeBase):
   pass
@@ -15,9 +13,7 @@ app = Flask(__name__, template_folder="templates")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 # initialize the app with the extension
 db.init_app(app)
-#app.config["SESSION_PERMANENT"] = False
-#app.config["SESSION_TYPE"] = "filesystem"
-#Session(app)
+
 
 # Tables
 class Resto(db.Model):
@@ -39,10 +35,6 @@ class Kunde(db.Model):
     password = db.Column(db.String(20), nullable = False)
     wallet = db.Column(db.Integer, nullable=False, default=200)
 
-class User:
-    def __init__(self, id, type):
-        self.id = id
-        self.type = type
 
 app.secret_key = "please end my suffering"
 
@@ -74,9 +66,9 @@ def registerKunde():
         db.session.add(new_user)
         db.session.commit()
         # todo replace user with primary key
-        user = vorname
+        id = 1
+        user = {"id": id, "type": "Kunde"}
         session["user"] = user
-        session["usertype"] = "Kunde"
     return render_template("index.html")
 
 @app.route("/registerResto", methods=["POST"])
@@ -100,43 +92,57 @@ def registerResto():
         db.session.add(new_resto)
         db.session.commit()
         # to do replace with primary key / possible to get primary key from commit?
-        user = name
-        print(user)
+        id = 1
+        user = {"id": id, "type": "Resto"}
         session["user"] = user
-        print(session["user"])
-        session["usertype"] = "Resto"
     return render_template("index.html")
 
 @app.route("/profile/")
 def profile():
     if not session.get("user"):
         return redirect(url_for("login"))
-    user = session["user"]
-    for kunde in db.session.query(Kunde).filter_by(vorname = user):
-        print(kunde)
-        user2 = getattr(kunde, "vorname")
+    user1 = session["user"]
+    print(user1)
+    user2 = None
+    if user1["type"] == "Kunde":
+        for kunde in db.session.query(Kunde).filter_by(id = user1["id"]):
+            user2 = getattr(kunde, "name")
+    else:
+        for resto in db.session.query(Resto).filter_by(id = user1["id"]):
+            user2 = getattr(resto, "name")
     print(user2)
-    return render_template("profile.html", content=[user+".name", user+".address", "blbllb", "wifbwb"])
-    # todo replace "content" with database results
+    return render_template("profile.html", content=user2, userType = user1["type"])
+    # todo update in profile html to deal with kunde type
     
     
 @app.route("/profile/update/", methods=["POST", "GET"])
 def profile_update():
-    if not "user" in session:
-        return redirect(url_for("login"))
-    if request.method == "GET":
-        return render_template("profile_update.html")
-    elif request.method == "POST":
-        new_username = request.form["new_username"]
-        session["user"] = new_username
-        # todo insert user in database
-        return redirect(url_for("profile"))
-    else:
-        return redirect(url_for("index"))
-        # error message?
+    user = session["user"]
+    if user["type"] == "Kunde":
+        if request.method == "POST":
+            name = request.form.get("name")
+            strasse = request.form.get("strasse")
+            plz = request.form.get("plz")
+            beschreibung = request.form.get("beschreibung")
+            password = request.form.get("password")
+            
+    else: 
+        if request.method == "POST":
+            name = request.form.get("name")
+            strasse = request.form.get("strasse")
+            plz = request.form.get("plz")
+            beschreibung = request.form.get("beschreibung")
+            password = request.form.get("password")
+            openTime = request.form.get("openTime")
+
+        # update user
+
+    return redirect(url_for("profile"))
 
     
 
+    
+# delete this
 @app.route("/login/", methods=["POST", "GET"])
 def login():
     if "user" in session:
@@ -144,8 +150,9 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
-        user = request.form["username"]
-        session["user"] = user
+        temp = request.form["username"]
+        user1 = {"id" : 1, "type" : "Kunde"}
+        session["user"] = user1
         return redirect(url_for("profile"))
     else:
         return redirect(url_for("index"))
@@ -164,14 +171,78 @@ def homepage():
     user = session["user"]
     return render_template("index_old.html", username = user)
 
+
 @app.route("/summary/")
 def summary():
     if not "user" in session:
         return redirect(url_for("login"))
-    # get items from session cache
-    selectedItems = []
-    # query to get all item details for items
-    return render_template("summary.html", selectedItems)
+    # only test
+    item1 = {"name" : "Name1", "description" : "Text1", "price" : 1.20, "amount" : 5}
+    item2 = {"name" : "Name2", "description" : "Text2", "price" : 2.20, "amount" : 4}
+    item3 = {"name" : "Name3", "description" : "Text3", "price" : 3.20, "amount" : 3}
+    
+    #if not "items" in session:
+        # no items selected
+    #    return redirect(url_for("index"))
+    items = [item1, item2, item3]
+    total = 0.0
+    for e in items:
+        total += e["price"]
+    final = f"{total:.2f}"
+    return render_template("summary.html", content = items, total = final)
+
+@app.route("/profile_alternative/", methods=["GET", "POST"])
+def profile_alternative():
+    if not "user" in session:
+        return redirect(url_for("login"))
+    user1 = session["user"]
+    if request.method == "GET":
+        if user1["type"] == "Kunde":
+            for kunde in db.session.query(Kunde).filter_by(id = user1["id"]):
+                content = {
+                    "vorname" : getattr(kunde, "vorname"),
+                    "nachname" : getattr(kunde, "nachname"),
+                    "adresse" : getattr(kunde, "adresse"),
+                    "postleitzahl" : getattr(kunde, "postleitzahl"),
+                    "password" : getattr(kunde, "password")
+                    }
+        else:
+            for resto in db.session.query(Resto).filter_by(id = user1["id"]):
+                content = { 
+                    "name" : getattr(kunde, "name"),
+                    "strasse" : getattr(kunde, "strasse"),
+                    "plz" : getattr(kunde, "plz"),
+                    "beschreibung" : getattr(kunde, "beschreibung"),
+                    "password" : getattr(kunde, "password"),
+                    "openTime" : getattr(kunde, "openTime")
+                    }
+        return render_template("profile_alternative.html", content = content, user1 = user1)
+    elif request.method == "POST":
+        if user1["type"] == "Kunde":
+            for user2 in db.session.query(Kunde).filter_by(id = user1["id"]):
+                print(user2)
+                if request.form.get("vorname"):
+                    user2.vorname = request.form.get("vorname")
+                if request.form.get("nachname"):
+                    user2.nachname = request.form.get("nachname")
+                if request.form.get("adresse"):
+                    user2.adresse = request.form.get("adresse")
+                if request.form.get("postleitzahl"):
+                    user2.postleitzahl = request.form.get("postleitzahl")
+                if request.form.get("password"):
+                    user2.password = request.form.get("password")
+                db.session.commit()
+                return redirect(url_for("profile_alternative"))
+        else:
+            name = request.form.get("name")
+            strasse = request.form.get("strasse")
+            plz = request.form.get("plz")
+            beschreibung = request.form.get("beschreibung")
+            password = request.form.get("password")
+            openTime = request.form.get("openTime")
+    else:
+        # should never be called
+        return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
