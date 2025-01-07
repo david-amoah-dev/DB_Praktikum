8dfbb7d366b77ce87c0a575bbf461617441552c8
saga <saghar.roshanavandi@stud.uni-due.de>
2025-01-07 16:07:48 +0100

    hgjk,

--- appSaghar.py
+++ appSaghar.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, render_template, request, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime, timezone
 
 class Base(DeclarativeBase):
   pass
@@ -43,6 +45,19 @@ class Kunde(db.Model):
 
     def __repr__(self):
        return f"Kunde('{self.nachname}')"
+    
+class Orders(db.Model):
+    # Zeit ohne Millisekunden
+    time = db.Column(DateTime, default=lambda: datetime.now().replace(microsecond=0))
+    id = db.Column(db.Integer, primary_key=True)
+    lieferstatus = db.Column(db.String(50), nullable=False, default = "in Bearbeitung")
+    items = db.Column(db.Text, nullable=False)  
+    preis = db.Column(db.Float, nullable=False)  
+    menge = db.Column(db.Integer, nullable=False) 
+    zahlungsstatus = db.Column(db.String(50), nullable=False, default = "ausstehend")
+    liefergebuehren = db.Column(db.Float, nullable=False)
+    gesamt = db.Column(db.Float, nullable=False)
+    anmerkungen = db.Column(db.Text, nullable=True)
     
 
 with app.app_context():
@@ -124,6 +139,14 @@ def login():
     
     return render_template('loginsaghar.html')
 
+@app.route('/bestellansicht', methods =["GET", "POST"])
+def bestellansicht():
+
+    orders = Orders.query.order_by(
+        # numbers to group orders by priority, time.desc() to sort by decending time after being grouped by their status
+        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2},value=Orders.lieferstatus),Orders.time.desc()).all()
+    return render_template('resto_Bestellansicht.html', orders=orders)
+
     
 @app.route('/restaurants', methods=["GET"])
 def CatchResto():
@@ -133,6 +156,62 @@ def CatchResto():
     #catch the restaurant with the same PLZ
     restaurants= Resto.query.filter_by(plz=kunde_postleitzahl).all()
     return render_template('restaurants.html', restaurants = restaurants)
+
+
+ # edit redirecting!!
+@app.route('/order/<int:order_id>/decline', methods=['POST'])
+def decline_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    order.lieferstatus = "storniert" 
+    order.zahlungsstatus = "abgebrochen"
+    db.session.commit()
+    return redirect(url_for('bestellansicht'))
+@app.route('/order/<int:order_id>/accept', methods=['POST'])
+def accept_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    order.lieferstatus = "in Zubereitung" 
+    order.zahlungsstatus = "abgeschlossen"
+    db.session.commit()
+    return redirect(url_for('bestellansicht'))
+@app.route('/order/<int:order_id>/finished', methods=['POST'])
+def finished_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    order.lieferstatus = "abgeschlossen" 
+    db.session.commit()
+    return redirect(url_for('bestellansicht'))
+
+
+@app.route('/add_order', methods=["POST"])
+def add_order():
+    if request.method == "POST":
+        lieferstatus = request.form.get("lieferstatus")
+        items = request.form.get("items")
+
+        # preis ist nicht static gespeichert!!
+        preis = float(request.form.get("preis"))
+
+        menge = int(request.form.get("menge"))
+        zahlungsstatus = request.form.get("zahlungsstatus")
+        liefergebuehren = float(request.form.get("liefergebuehren"))
+        anmerkungen = request.form.get("anmerkungen")
+
+        # muss noch angepasst werden
+        gesamt = preis * menge + liefergebuehren
+
+        new_order = Orders(
+            lieferstatus=lieferstatus,
+            items=items,
+            preis=preis,
+            menge=menge,
+            zahlungsstatus=zahlungsstatus,
+            liefergebuehren=liefergebuehren,
+            gesamt=gesamt,
+            anmerkungen = anmerkungen
+        )
+        db.session.add(new_order)
+        db.session.commit()
+
+        return redirect(url_for('bestellansicht'))
     
 if __name__ == '__main__':
     app.run(debug=True)
