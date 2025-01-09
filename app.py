9fd897d5d21a0623ae99c5b6ff959c615965c65b
david-amoah-dev <david_julian_amoah@outlook.de>
2025-01-09 16:19:26 +0100

    Bestellhistorie in Code eingebettet auch hier noch überarbeitungs bedarf

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import sqlite3
 from flask import Flask, url_for, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import DateTime
+from datetime import datetime, timezone
+
 
 class Base(DeclarativeBase):
     pass
@@ -51,15 +54,20 @@ class Item(db.Model):
     # bild = db.Column()
     category = db.Column(db.String(), nullable=True)
 
-class Order(db.Model):
+class Orders(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    ordrtitle = db.Column(db.String(20), unique = False, nullable = False)
-    # status = db.Column()
-    # item = db.Column()
-    # customer = db.Column()
-    # datetime = db.Column()
-    comment = db.Column(db.String(200), unique = False, nullable = False)
+    ordrtitle = db.Column(db.String(20), unique = False, nullable = True)
+    time = db.Column(DateTime, default=lambda:datetime.now().replace(microsecond=0))
+    lieferstatus = db.Column(db.String(50), nullable=False, default = "in Bearbeitung")
+    items = db.Column(db.Text, nullable=False)
+    preis = db.Column(db.Float, nullable=False)
+    menge = db.Column(db.Integer, nullable=False)
+    zahlungsstatus = db.Column(db.String(50), nullable=False, default = "ausstehend")
+    liefergebuehren = db.Column(db.Float, nullable=True)
+    gesamt = db.Column(db.Float, nullable=False)
+    anmerkungen = db.Column(db.String(200), unique = False, nullable = True)
     # ammountOfItem = db.Column(db.Integer(), nullable = False)
+    # customer = db.Column()
 
 # Page routes----------------------------------------------------------------------------------
 @app.route("/", methods=["GET"])
@@ -87,7 +95,10 @@ def cstmpage():
 
 @app.route("/cstmbsth")
 def cstmbsth():
-    return render_template('CustomerView-Bestellhistorie.html')
+    orders = Orders.query.order_by(
+        # numbers to group orders by priority, time.desc() to sort by decending time after being grouped by their status
+        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2},value=Orders.lieferstatus),Orders.time.desc()).all()
+    return render_template('CustomerView-Bestellhistorie.html', orders = orders)
 
 @app.route("/cstmrstdtl")
 def cstmrstdtl():
@@ -165,6 +176,80 @@ def ordradd():
         db.session.add(new_order)
         db.session.commit()
     return redirect(url_for("rstrspkt"))
+
+#___________________________________________Dinhs routes___________________________________
+@app.route('/bestellansicht', methods =["GET", "POST"])
+def bestellansicht():
+
+    orders = Orders.query.order_by(
+        # numbers to group orders by priority, time.desc() to sort by decending time after being grouped by their status
+        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2},value=Orders.lieferstatus),Orders.time.desc()).all()
+    return render_template('RestaurantView-Bestellhistorie.html', orders = orders)
+
+#________________________________________
+
+@app.route('/order/<int:order_id>/decline', methods=['POST'])
+def decline_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    order.lieferstatus = "storniert"
+    order.zahlungsstatus = "abgebrochen"
+    db.session.commit()
+    return redirect(url_for('bestellansicht'))
+@app.route('/order/<int:order_id>/accept', methods=['POST'])
+def accept_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    order.lieferstatus = "in Zubereitung"
+    order.zahlungsstatus = "abgeschlossen"
+    db.session.commit()
+    return redirect(url_for('bestellansicht'))
+@app.route('/order/<int:order_id>/finished', methods=['POST'])
+def finished_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    order.lieferstatus = "abgeschlossen"
+    db.session.commit()
+    return redirect(url_for('bestellansicht'))
+
+# ____________________________________________
+
+@app.route('/add_order', methods=["POST"])
+def add_order():
+    if request.method == "POST":
+        #if "user" in session:
+         #   user = session["user"]
+          #  if user["type"] == "Kunde":
+            # Fetch the Kunde user from the database using the stored ID
+        #        kunde = Kunde.query.get_or_404(user["id"])
+
+        #name = {kunde.nachname, kunde.vorname}
+        #adresse = kunde.adresse
+        lieferstatus = request.form.get("lieferstatus")
+        items = request.form.get("items")
+
+        # preis ist nicht static gespeichert!!
+        preis = float(request.form.get("preis"))
+
+        menge = int(request.form.get("menge"))
+        zahlungsstatus = request.form.get("zahlungsstatus")
+        liefergebuehren = float(request.form.get("liefergebuehren"))
+        anmerkungen = request.form.get("anmerkungen")
+
+        # muss noch angepasst werden
+        gesamt = preis * menge + liefergebuehren
+
+        new_order = Orders(
+            lieferstatus=lieferstatus,
+            items=items,
+            preis=preis,
+            menge=menge,
+            zahlungsstatus=zahlungsstatus,
+            liefergebuehren=liefergebuehren,
+            gesamt=gesamt,
+            anmerkungen = anmerkungen
+        )
+        db.session.add(new_order)
+        db.session.commit()
+
+        return redirect(url_for('bestellansicht'))
 
 # __________________________________________saghars routes___________________________________
 
