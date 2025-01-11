3231155a3fbb398053b2e806231e2f00011c5c05
saga <saghar.roshanavandi@stud.uni-due.de>
2025-01-11 20:27:05 +0100

    changed the bestellhistorie

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase
 from datetime import datetime, timezone
-import json
 
 class Base(DeclarativeBase):
   pass
@@ -54,13 +53,22 @@ class Orders(db.Model):
     lieferstatus = db.Column(db.String(50), nullable=False, default = "in Bearbeitung")
     items = db.Column(db.Text, nullable=False)  
     preis = db.Column(db.Float, nullable=False)  
+    menge = db.Column(db.Integer, nullable=False) 
     zahlungsstatus = db.Column(db.String(50), nullable=False, default = "ausstehend")
+    liefergebuehren = db.Column(db.Float, nullable=False)
+    gesamt = db.Column(db.Float, nullable=False)
+    anmerkungen = db.Column(db.Text, nullable=True)
     name = db.Column(db.Text, nullable=False) 
     adresse = db.Column(db.Text, nullable=False) 
-    kunde_id = db.Column(db.Integer, nullable=False)
-    resto_id = db.Column(db.Integer, nullable=False)
-    anmerkungen = db.Column(db.Text, nullable = True)
-    postleitzahl = db.Column(db.Text, nullable=False)
+    #restaurant id beinhalten
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    itmname = db.Column(db.String(20), unique = False, nullable = True)
+    description = db.Column(db.String(20), unique = False, nullable = True)
+    price = db.Column(db.Integer(), nullable = True)
+    
+    
 
 with app.app_context():
   db.create_all()
@@ -137,7 +145,7 @@ def login():
                 'plz': kunde.postleitzahl,
                 'username': kunde.nachname
             }
-            return redirect(url_for('bestellansichtKunde'))#showa the restaurants if any available
+            return redirect(url_for('CatchResto'))#showa the restaurants if any available
         elif resto and resto.password == password:
             user = {"id" : resto.id, "type" : "Resto", "plz" : resto.plz, "username" : resto.name}
             session["user"] = user
@@ -152,38 +160,18 @@ def login():
 @app.route('/bestellansichtKunde', methods =["GET", "POST"])
 def bestellansichtKunde():
 
-    if 'user' not in session or session['user']['type'] != 'Kunde':
-        return redirect(url_for('login'))
-
-    kunde_id = session['user']['id']
-
-    # Query orders specific to the logged-in customer, ordered by priority and time
-    orders = Orders.query.filter_by(kunde_id=kunde_id).order_by(
-        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2}, 
-                value=Orders.lieferstatus),
-        Orders.time.desc()
-    ).all()
-
-    return render_template('kunde_Bestellansicht.html', orders=orders, json=json)
-
-
+    orders = Orders.query.order_by(
+        # numbers to group orders by priority, time.desc() to sort by decending time after being grouped by their status
+        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2},value=Orders.lieferstatus),Orders.time.desc()).all()
+    return render_template('resto_Bestellansicht.html', orders = orders)
 
 @app.route('/bestellansichtResto', methods =["GET", "POST"])
 def bestellansichtResto():
-    if 'user' not in session or session['user']['type'] != 'Resto':
-        return redirect(url_for('login'))
 
-    resto_id = session['user']['id']
-
-    # Query orders specific to the logged-in customer, ordered by priority and time
-    orders = Orders.query.filter_by(resto_id=resto_id).order_by(
-        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2}, 
-                value=Orders.lieferstatus),
-        Orders.time.desc()
-    ).all()
-
-    return render_template('resto_Bestellansicht.html', orders=orders, json=json)
-
+    orders = Orders.query.order_by(
+        # numbers to group orders by priority, time.desc() to sort by decending time after being grouped by their status
+        db.case({"in Bearbeitung": 1, "in Zubereitung": 1, "abgeschlossen": 2, "storniert": 2},value=Orders.lieferstatus),Orders.time.desc()).all()
+    return render_template('resto_Bestellansicht.html', orders = orders)
     
 @app.route('/restaurants', methods=["GET"])
 def CatchResto():
@@ -201,6 +189,52 @@ def OpenRestaurant(restaurant_id):
     restaurant = Resto.query.get_or_404(restaurant_id)
 
     return render_template('RestaurantView-Speisekarte.html', restaurant=restaurant)
+
+
+@app.route("/itmadd", methods = ['POST'])
+def itmadd():
+    if request.method == "POST":
+        itmname = request.form.get("itemname")
+        description = request.form.get("description")
+        price = request.form.get("price")
+        category = request.form.get("category")
+
+        # create Item
+        new_item = Item(itmname = itmname,description = description,price = price,category = category)
+        print(f"Received: {itmname}, {description}, {price}, {category}")
+        db.session.add(new_item)
+        db.session.commit()
+    return redirect(url_for("rstrspkt"))
+
+@app.route("/delitm/<int:mid>", methods = ['GET','POST'])
+def delitm(mid):
+    item = db.session.execute(db.select(Item).filter_by(id = mid)).scalar_one()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for("rstrspkt"))
+
+@app.route("/upditm/<int:updid>", methods = ['GET','POST'])
+def upditm(updid):
+    item = db.session.execute(db.select(Item).filter_by(id = updid)).scalar_one()
+    if request.method == "POST":
+        upitmname = request.form.get("upitemname")
+        updescription = request.form.get("updescription")
+        upprice = request.form.get("upprice")
+        upcategory = request.form.get("upcategory")
+
+        # update Item
+
+        updated_item = Item.query.get_or_404(updid)
+
+        updated_item.itmname = upitmname
+        updated_item.description = updescription
+        updated_item.price = upprice
+        updated_item.category = upcategory
+
+        print(f"Received: {upitmname}, {updescription}, {upprice}, {upcategory}")
+        #db.session.update(item)
+        db.session.commit()
+    return redirect(url_for("rstrspkt"))
 
 
 
@@ -269,36 +303,20 @@ def decline_order(order_id):
     order.lieferstatus = "storniert" 
     order.zahlungsstatus = "abgebrochen"
     db.session.commit()
-    return redirect(request.referrer)
+    return redirect(url_for('bestellansicht'))
 @app.route('/order/<int:order_id>/accept', methods=['POST'])
 def accept_order(order_id):
     order = Orders.query.get_or_404(order_id)
-    user = Kunde.query.get_or_404(order.kunde_id)
-    resto = Resto.query.get_or_404(order.resto_id)
-
-    #update User Wallet
-    userWallet = float(user.wallet)
-    userWallet -= float(order.preis)
-    finalUserWallet = f"{userWallet:.2f}"
-    user.wallet = finalUserWallet
-
-    #update Resto Wallet
-    restoWallet = float(resto.wallet)
-    restoWallet += (float(order.preis) * 0.85) # remove cut for lfrsptz
-    finalRestoWallet = f"{restoWallet:.2f}"
-    resto.wallet = finalRestoWallet
-
-    # update order status
     order.lieferstatus = "in Zubereitung" 
     order.zahlungsstatus = "abgeschlossen"
     db.session.commit()
-    return redirect(request.referrer)
+    return redirect(url_for('bestellansicht'))
 @app.route('/order/<int:order_id>/finished', methods=['POST'])
 def finished_order(order_id):
     order = Orders.query.get_or_404(order_id)
     order.lieferstatus = "abgeschlossen" 
     db.session.commit()
-    return redirect(request.referrer)
+    return redirect(url_for('bestellansicht'))
 
 
 @app.route('/add_order', methods=["POST"])
@@ -347,65 +365,24 @@ def add_order():
     
 @app.route("/summary/")
 def summary():
-    # to do change to get items from session
-    item1 = {"name" : "Name1", "description" : "Text1", "price" : 1.20, "amount" : 5, "restoID" : 1}
-    item2 = {"name" : "Name2", "description" : "Text2", "price" : 2.20, "amount" : 4, "restoID" : 1}
-    item3 = {"name" : "Name3", "description" : "Text3", "price" : 3.20, "amount" : 3, "restoID" : 1}
-    items = [item1, item2, item3]
-    session["items"] = items
-
     if not "user" in session:
         return redirect(url_for("login"))
-#   if not "items" in session:
-#       return redirect(url_for("index"))
-#   items = session["items"]
-
-    # calculate price 
+    # to do change to get items from session
+    item1 = {"name" : "Name1", "description" : "Text1", "price" : 1.20, "amount" : 5}
+    item2 = {"name" : "Name2", "description" : "Text2", "price" : 2.20, "amount" : 4}
+    item3 = {"name" : "Name3", "description" : "Text3", "price" : 3.20, "amount" : 3}
+    
+    #if not "items" in session:
+        # no items selected
+    #    return redirect(url_for("index"))
+    items = [item1, item2, item3]
     total = 0.0
-    for elem in items:
-        total += elem["price"] * elem["amount"]
+    for e in items:
+        total += e["price"]
     final = f"{total:.2f}"
-
     return render_template("summary.html", content = items, total = final)
 
-@app.route("/new_order/", methods=["POST"])
-def new_order():
-    if not "user" in session:
-        return redirect(url_for("login"))
-    if not "items" in session:
-        return redirect(url_for("login"))
-    # get session elements
-    userID = session["user"]["id"]
-    items = session["items"]
-    restoID = items[0]["restoID"]
-
-    user = Kunde.query.get_or_404(userID)
-
-    # calculate price
-    total = 0.0
-    for elem in items:
-        total += elem["price"] * elem["amount"]
-    finalprice = f"{total:.2f}"
-
-    # serialize Items
-    serializedItems = json.dumps(items) 
-
-    new_order = Orders(
-        lieferstatus = "in Bearbeitung",
-        items = serializedItems,
-        preis = float(finalprice),
-        zahlungsstatus = "ausstehend",
-        name = f"{user.vorname} {user.nachname}",
-        adresse = user.adresse,
-        kunde_id = userID,
-        resto_id = restoID,
-        anmerkungen = request.form.get("anmerkungen"),
-        postleitzahl = user.postleitzahl
-    )
-    db.session.add(new_order)
-    db.session.commit()
     
-    return redirect(url_for("bestellansichtKunde"))
 
 @app.route('/logout', methods=['POST'])
 def logout():
@@ -424,10 +401,3 @@ def test( restaurant_id):
     
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-# Item Ansicht in Bestellansicht updaten -> veränderte Tabellenstruktur Orders
-# Input session["Items"]
-#Popup bei einkommender Bestellung oder weiterleitung an Bestellansicht?
